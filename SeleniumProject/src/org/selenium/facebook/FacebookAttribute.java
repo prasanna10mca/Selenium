e36eb556beaa833b405f3084983e49093819e93c
Prasanna <prasanna10mca@gmail.com>
2021-12-23 23:46:52 +0530

    program modified

--- SeleniumProject/src/org/selenium/facebook/FacebookAttribute.java
+++ SeleniumProject/src/org/selenium/facebook/FacebookAttribute.java
@@ -7,7 +7,7 @@
 	import org.openqa.selenium.WebElement;
 	import org.openqa.selenium.chrome.ChromeDriver;
 
-	public class SeleniumFacebookAttribute {
+	public class FacebookAttribute {
 
 		public static void main(String[] args) {
 			
---
+++ SeleniumProject/src/org/selenium/tutorial/MyDragAndDrop.java
@@ -0,0 +1,10 @@
+package org.selenium.tutorial;
+
+public class MyDragAndDrop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
